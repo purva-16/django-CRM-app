56661a0c017b9cf91eb30b4b22b97a8c53f29129
purva-16 <pb2330@srmist.edu.in>
2024-01-17 02:31:44 +0530

    11th commit- add new records

--- dcrm/website/urls.py
+++ dcrm/website/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('register/', views.register_user, name='register'),
     path('record/<int:pk>', views.customer_record, name='record'),
     path('delete_record/<int:pk>', views.delete_record, name='delete'),
+    path('add_record/', views.add_record, name='add_record'),
 
 
 
