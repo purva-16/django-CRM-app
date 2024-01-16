44eee217b4fa62a054fcec14afcaa8d9fbf1a055
purva-16 <pb2330@srmist.edu.in>
2024-01-16 21:21:42 +0530

    6th commit - register users

--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate , login , logout 
 from django.contrib import messages  
+from . forms import SignUpForm
 
 # Create your views here.
 def home(request):
@@ -26,3 +27,21 @@ def logout_user(request):
     logout(request)
     messages.success(request , " you have been logged out....")
     return redirect('home')
+
+
+
+def register_user(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username , password = password)
+            login(request , user)
+            messages.success(request , " You Have Successfully Registered!")
+            return redirect('home')
+    else:
+            form = SignUpForm        
+            return render(request , 'register.html', {'form':form})
+    return render(request , 'register.html', {'form':form})
